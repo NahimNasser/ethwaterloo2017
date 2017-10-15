de02d4c7bc3e306b3460f10305719df6ea730229
moejay <ali.jajeh@gmail.com>
2017-10-14 20:45:48 -0400

    Parent creator contract

--- contracts/GitBounty.sol
+++ contracts/GitBounty.sol
@@ -1,8 +1,42 @@
 pragma solidity ^0.4.2;
+contract GitBountyCreator {
+    struct Bounty {
+        bool isHere;
+        address addr;
+        uint256 voteProgress;
+        uint256 totalVoters;
+        uint256 payoutAmount;
+    }
+    mapping (string => Bounty) private bounties;
+    modifier isChild(string key) {
+        require(bounties[key].isHere);
+        _;
+    }
+    function createBounty(string issueUrl, address[] voters,uint256  expiresIn ) public payable {
+        GitBounty b = new GitBounty(issueUrl, voters, expiresIn, this);
+        bounties[issueUrl] = Bounty({
+            isHere: true,
+            addr: b,
+            voteProgress: 0,
+            totalVoters: voters.length,
+            payoutAmount: 0
+        });
+        b.addToBounty.value(msg.value)();
+
+    }
+    function getBounty(string key) public constant returns (address, uint256, uint256, uint256) {
+        return (bounties[key].addr, bounties[key].voteProgress, bounties[key].totalVoters, bounties[key].payoutAmount);
+    }
+    function updateBountyAmount(string key, uint256 newAmount) public isChild(key) {
+        Bounty storage b = bounties[key];
+        b.payoutAmount = newAmount;
+        bounties[key] = b;
+    }
+}
 
 contract GitBounty {
-
-    bytes32 public key;
+    GitBountyCreator public parent;
+    string public key;
     address public owner;
     uint256 public totalBounty;
     uint256 public expiresAt;
@@ -30,7 +64,8 @@ contract GitBounty {
         require(!hasVoted(voter, hunter));
         _;
     }
-    function GitBounty(bytes32 issueUrl, address[] voters,uint256  expiresIn ) public payable {
+    function GitBounty(string issueUrl, address[] voters,uint256  expiresIn, GitBountyCreator parentAddress ) public payable {
+        parent = parentAddress;
         key = issueUrl;
         isBountyOpen = true;
         expiresAt += expiresIn + now;
@@ -46,6 +81,7 @@ contract GitBounty {
     function addToBounty() public payable bountyOpen {
         contributions[msg.sender] += msg.value;
         totalBounty += msg.value;
+        parent.updateBountyAmount(key, totalBounty);
     }
     function vote(address addr) public isEligibleVoter bountyOpen votedOnce(msg.sender, addr){
         votes[addr] += 1;
